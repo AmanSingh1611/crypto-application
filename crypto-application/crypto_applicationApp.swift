c9ff8a7e16c697b7dcf6bfa59bcea1510fd17559
Aman Singh <aman16024375@gmail.com>
2025-10-24 12:51:36 +0530

    Initial Commit

---
+++ crypto-application/crypto_applicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  crypto_applicationApp.swift
+//  crypto-application
+//
+//  Created by Aman on 24/10/25.
+//
+
+import SwiftUI
+
+@main
+struct crypto_applicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
