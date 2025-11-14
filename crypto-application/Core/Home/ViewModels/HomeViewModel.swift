ff147e0cffaff4d1aa2ae1df1c63b844d442df4e
Aman Singh <amans@advantageclub.ai>
2025-11-14 12:40:49 +0530

    Feature: Added statistic section in home page

--- crypto-application/Core/Home/ViewModels/HomeViewModel.swift
+++ crypto-application/Core/Home/ViewModels/HomeViewModel.swift
@@ -11,6 +11,7 @@ internal import UIKit
 
 class HomeViewModel: ObservableObject {
     
+    @Published var statistics: [StatisticModel] = []
     @Published var allCoins: [CoinModel] = []
     @Published var portfolioCoins: [CoinModel] = []
     @Published var isLoading: Bool = false
@@ -18,6 +19,7 @@ class HomeViewModel: ObservableObject {
     @Published var sortOption: SortOption = .holdings
         
     private let coinDataService = CoinDataService()
+    private let marketDataService = MarketDataService()
     private var cancellables = Set<AnyCancellable>()
     
     enum SortOption {
@@ -41,6 +43,15 @@ class HomeViewModel: ObservableObject {
             .map(filterAndSortCoins)
             .sink { [weak self] (returnedCoins) in
                 self?.allCoins = returnedCoins
+            }
+            .store(in: &cancellables)
+        
+        marketDataService.$marketData
+            .combineLatest($portfolioCoins)
+            .map(mapGlobalMarketData)
+            .sink { [weak self] (returnedStats) in
+                self?.statistics = returnedStats
+                self?.isLoading = false
             }
             .store(in: &cancellables)
     }
@@ -81,5 +92,50 @@ class HomeViewModel: ObservableObject {
         case .priceReversed:
             coins.sort(by: { $0.currentPrice < $1.currentPrice })
         }
+    }
+    
+    private func mapGlobalMarketData(marketDataModel: MarketDataModel?, portfolioCoins: [CoinModel]) -> [StatisticModel] {
+        var stats: [StatisticModel] = []
+
+        guard let data = marketDataModel else {
+            return stats
+        }
+        
+        let marketCap = StatisticModel(title: "Market Cap", value: data.marketCap, percentageChange: data.marketCapChangePercentage24HUsd)
+        let volume = StatisticModel(title: "24h Volume", value: data.volume)
+        let btcDominance = StatisticModel(title: "BTC Dominance", value: data.btcDominance)
+        
+        let portfolioValue =
+            portfolioCoins
+                .map({ $0.currentHoldingsValue })
+                .reduce(0, +)
+        
+        let previousValue =
+            portfolioCoins
+                .map { (coin) -> Double in
+                    let currentValue = coin.currentHoldingsValue
+                    let percentChange = coin.priceChangePercentage24H ?? 0 / 100
+                    let previousValue = currentValue / (1 + percentChange)
+                    return previousValue
+                }
+                .reduce(0, +)
+
+        var percentageChange: Double? {
+            guard previousValue != 0 else { return nil }
+            return (portfolioValue - previousValue) / previousValue
+        }
+        
+        let portfolio = StatisticModel(
+            title: "Portfolio Value",
+            value: portfolioValue.asCurrencyWith2Decimals(),
+            percentageChange: percentageChange)
+        
+        stats.append(contentsOf: [
+            marketCap,
+            volume,
+            btcDominance,
+            portfolio
+        ])
+        return stats
     }
 }
