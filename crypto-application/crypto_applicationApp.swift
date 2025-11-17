a4ca5686d2036c6654af517c25169929668cbea3
Aman Singh <amans@advantageclub.ai>
2025-11-17 13:19:16 +0530

    Feature: Added portfolio data service and starage login in core data

--- crypto-application/crypto_applicationApp.swift
+++ crypto-application/crypto_applicationApp.swift
@@ -10,6 +10,13 @@ import SwiftUI
 @main
 struct crypto_applicationApp: App {
     @StateObject private var homeViewModel = HomeViewModel()
+    
+    init() {
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor : UIColor(Color.theme.accent)]
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor : UIColor(Color.theme.accent)]
+        UINavigationBar.appearance().tintColor = UIColor(Color.theme.accent)
+    }
+    
     var body: some Scene {
         WindowGroup {
             NavigationView {
